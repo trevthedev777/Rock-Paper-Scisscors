d0b00b5b9702c13b5e2d3ed2c9ff06deeb56ae95
Your Name <you@example.com>
2022-04-12 11:07:35 +0200

    add basic conditional logic to let the game run

---
+++ rock_paper_scissors.py
@@ -0,0 +1,19 @@
+print('Rock...')
+print('Paper...')
+print('Scissors...')
+
+player_one = input('Player 1, make your move: ')
+player_two = input('Player 2, make your move: ')
+
+if player_one == 'rock' and player_two == 'scissors':
+    print('Player 1 wins!')
+elif player_one == 'rock' and player_two == 'paper':
+    print('Player 2 wins')
+elif player_one == 'paper' and player_two == 'rock':
+    print('Player 1 wins')
+elif player_one == 'paper' and player_two == 'scissors':
+    print('Player 2 wins')
+elif player_one == player_two:
+    print('Draw, Go again')
+else:
+    print('Something went wrong, Please try again')
